40c0d306cdb517c19eb3ee1220f31304cc9e98da
Jonse codez <65801469+Jonse178@users.noreply.github.com>
2021-08-30 05:44:54 +0000

    Finished Episode 3

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-#Time to learn how to host the bot!
+#Welcome to Episode 3!
+#Introduction to commands!
 import os
 import discord#pip install discord
+from discord.ext import commands
 from keep_alive import keep_alive#import the webserver file
 
-bot = discord.Client()
+bot = commands.Bot(  command_prefix = '!'#Set the command_prefix
+                    ,case_insensitive=True#Make commands case insensitive
+                    )
 
 #When bot is ready
 @bot.event
@@ -16,12 +20,22 @@ async def on_message(message):
     await message.channel.send(f'Hello I am `{bot.user.name}`')#bot replies with this
   elif message.content.startswith('invite'):
     await message.channel.send(f'https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=0&scope=bot')#bot's invite from developer portal
-    
+
+  await bot.process_commands(message)#This fixes the bug
+
+#I forgot to mention when using on_message with commands there is a small bug. The on_message stops the commands.
+bot.remove_command('help')#removes the default help command
+
+@bot.command()
+async def hello(ctx):
+  await ctx.send('Hi')  
+
+@bot.command()
+async def help(ctx):
+  await ctx.send(f"{bot.user.name}'s Commands!\nHelp - Shows this message\nHello - Bot says Hello Back!")
 
 token = os.getenv('token')
 keep_alive()#call the webserver function
 bot.run(token)#run the bot
 
-#The webserver alone can keep the bot online for 1 hour
-#But how to make it 24/7 online?, you may ask
-#To make it 24/7 need to visit uptimerobot.com
+#Next episode I will teach on Embeds
